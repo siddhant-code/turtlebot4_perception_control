9457fba94af8666c4e6661a6fc2ee80b7966a64e
Siddhant <siddhant.deshmukh.mail@gmail.com>
2025-04-26 01:45:55 -0400

    changes

--- enpm673_final_proj/enpm673_module/paper_orientation_detection.py
+++ enpm673_final_proj/enpm673_module/paper_orientation_detection.py
@@ -4,7 +4,7 @@
 Find orientation of paper
 Note : Handle case if multiple pages with different orientation in frame. 
 
-Input: Preprocessed Frame/Masked frame/Frame from video (Decide what kind of frame would be useful)
+Input: Preprocessed Frame/Masked frame/Frame from video (Decide what kind of frame would be useful).
 Output: 
     x,y,theta corresponding to the center of paper, theta being the angle subtented by longer edge of paper.(May need to use projection and homography)
 
